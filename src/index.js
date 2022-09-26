135015484bba419cf6e2105bdddd3598d36ef4b8
Rail <wanglifei@yiqicai.com>
2022-09-26 19:36:31 +0800

    feat: ...

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const fs = require('fs')
+const fse = require('fs-extra')
 const parser = require('@babel/parser')
 const traverse = require('@babel/traverse').default
 const generate = require('@babel/generator').default
+const types = require('@babel/types')
 const fg = require('fast-glob')
 
 async function scan() {
   const files = await fg(['test/*.js'], {
-    ignore: ['*.test.js'],
+    ignore: ['test/*.test.js'],
     absolute: true,
   })
   return files
@@ -14,25 +16,32 @@ async function scan() {
 
 scan()
 
-function parse(file) {
+async function parse(file) {
   const sourceCode = fs.readFileSync(file, { encoding: 'utf8', flag: 'r' })
   const ast = parser.parse(sourceCode, {
     sourceType: 'unambiguous',
+    plugins: ['jsx'],
   })
   traverse(ast, {
     CallExpression(path, state) {
+      if (types.isMemberExpression(path.node.callee)) {}
     },
   })
   const { code, map } = generate(ast)
-  fs.writeFile('./cloneIndex.js', code)
+  if (await isExist('dist/test.js'))
+    return
+  console.log(code)
+  fse.outputFile('dist/test.js', code)
+}
+
+async function isExist(filePath) {
+  return await fse.emptyDir(filePath)
 }
 
 async function run() {
   const files = await scan()
-  for (let i = 0; i < files.length; i++) {
+  for (let i = 0; i < files.length; i++)
     parse(files[i])
-    console.log(files[i])
-  }
 }
 run()
 
--- test/index.js
+++ test/index.js
@@ -1,50 +1,14 @@
-function clone(item) {
-    if (!item) { return item; } // null, undefined values check
+    console.log(1);
 
-    var types = [ Number, String, Boolean ], 
-        result;
-
-    // normalizing primitives if someone did new String('aaa'), or new Number('444');
-    types.forEach(function(type) {
-        if (item instanceof type) {
-            result = type( item );
-        }
-    });
-
-    if (typeof result == "undefined") {
-        if (Object.prototype.toString.call( item ) === "[object Array]") {
-            result = [];
-            item.forEach(function(child, index, array) { 
-                result[index] = clone( child );
-            });
-        } else if (typeof item == "object") {
-            // testing that this is DOM
-            if (item.nodeType && typeof item.cloneNode == "function") {
-                result = item.cloneNode( true );    
-            } else if (!item.prototype) { // check that this is a literal
-                if (item instanceof Date) {
-                    result = new Date(item);
-                } else {
-                    // it is an object literal
-                    result = {};
-                    for (var i in item) {
-                        result[i] = clone( item[i] );
-                    }
-                }
-            } else {
-                // depending what you would like here,
-                // just keep the reference, or create new object
-                if (false && item.constructor) {
-                    // would not advice to do that, reason? Read below
-                    result = new item.constructor();
-                } else {
-                    result = item;
-                }
-            }
-        } else {
-            result = item;
-        }
+    function func() {
+        console.info(2);
     }
 
-    return result;
-}
+    export default class Clazz {
+        say() {
+            console.debug(3);
+        }
+        render() {
+            return <div>{console.error(4)}</div>
+        }
+    }
